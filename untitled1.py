f684e14d3fe7cab7198ba12a3cc019870b212d6f
melanie197 <74988083+melanie197@users.noreply.github.com>
2020-12-22 15:56:53 -0500

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,6 @@
+nombre= input("¿Cual es su primer nombre?");
+apellido=input("¿Cual es su apellido?");
+edad= input("¿Que edad tiene?");
+ciudad= input("¿DONDE VIVE?");
+space=" "
+print("Hola mi nombre es ",nombre,space,apellido,space,"Tengo",edad,"años" ,space,"Y vivo en la ciudad de ",ciudad)
